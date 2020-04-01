651c5b019e06b81d96d648a26791e386b9aa96ea
raheems <raheem@gmail.com>
2020-03-31 20:44:29 -0400

    bangladesh R code updated

--- covid19_bd.R
+++ covid19_bd.R
@@ -1,10 +1,22 @@
 # Mon Mar 30 19:53:38 2020 ------------------------------
-# Bangladesh unofficial
+# Bangladesh unofficial and official
 
-# install.packages('gsheet')
+# Load packages
+require(ggplot2)
+require(ggrepel)
+require(tidyverse)
+require(lubridate)
+require(knitr)
+require(kableExtra)
+
 library(gsheet)
-library(tidyverse)
-library(plotly)
+
+
+# Update Bangladesh unofficial and official data
+
+sheet_3 = 'https://docs.google.com/spreadsheets/d/1nlAQffAvqChLtvGiJvJPnNOJJKBu_uzmnKdpAJXuPwM/edit#gid=336445634'
+df = gsheet2tbl(sheet_3)
+write_csv(df, 'covid19_bd.csv')
 
 # RUN covid_update_data.R to update the data
 
