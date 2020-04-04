82eb8cdb6c1ecb657268beebaa4d69db9f39ea17
raheems <raheem@gmail.com>
2020-04-03 22:19:01 -0400

    added global case fatality

--- global_incidence_recovery_deaths.R
+++ global_incidence_recovery_deaths.R
@@ -20,7 +20,45 @@ jhu_deaths_global <- read_csv(jhu_deaths_global_src)
 jhu_recovered_global_src <- paste("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv", sep = "")
 jhu_recovered_global <- read_csv(jhu_recovered_global_src)
 
-# write_csv(jhu_confirmed_global, "jhu_confirmed_global.csv")
+
+tryCatch(
+  expr ={
+    # Confirmed
+    jhu_confirmed_global_src <- paste("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv", sep = "")
+    jhu_confirmed_global <- read_csv(jhu_confirmed_global_src)
+    write_csv(jhu_confirmed_global, "jhu_confirmed_global.csv")
+  },
+  error = function(e){
+    message('Caught an error!')
+    print(e)
+  },
+  warning = function(w){
+    message('Caugnt an warning!')
+    print(w)
+  },
+  finally = {
+    jhu_confirmed_global <- read_csv("jhu_confirmed_global.csv")
+  }
+
+)
+
+# Confirmed
+
+df_confirmed <- jhu_confirmed_global %>%
+  rename(province = "Province/State",
+         country = "Country/Region") %>%
+  pivot_longer(-c(province, country, Lat, Long),
+               names_to = "date", values_to = "cum_cases") %>%
+  mutate(
+    date = mdy(date)
+  ) %>%
+  select(country, date, cum_cases) %>%
+  group_by(country, date) %>%
+  summarize(
+    cum_cases = sum(cum_cases)
+  ) %>%
+  rename(cum_confirmed = cum_cases)
+
 
 # Create working data set excluding China
 df_deaths <- jhu_deaths_global %>%
@@ -56,16 +94,24 @@ df_recov <- jhu_recovered_global %>%
   rename(cum_recov = cum_cases)
 
 df <- left_join(df_deaths, df_recov, by = c("country", "date"))
+df <- left_join(df, df_confirmed, by = c("country", "date"))
 
 df <- df %>%
   mutate(
-    cum_recov_death_ratio = cum_recov/cum_deaths
+    daily_confirmed = c(0, diff(cum_confirmed))
+    , daily_recov = c(0, diff(cum_recov))
+    , daily_deaths = c(0, diff(cum_deaths))
+    , cum_recov_death_ratio = cum_recov/cum_deaths
   )
 
 
 # Last date updated or last reported date
 data_last_refreshed =  max(ymd(df$date))
 
-
-
+# df %>% filter(country == 'United Kingdom') %>%
+#   group_by(country) %>%
+#   summarise(
+#     recov = max(cum_recov)
+#     , deaths = max(cum_deaths)
+#   )
 
--- sandbox.R
+++ sandbox.R
@@ -32,3 +32,67 @@ ggplot(df_ny, aes(x= lag(daily_cases, 8), y = daily_deaths ))+
 
 summary(df_ny$daily_cases)
 boxplot(df_ny$daily_cases)
+
+
+# Recovery to death ratio
+filter(country %in% c('US', 'Bangladesh'))
+
+a <- df %>%
+  mutate(
+    daily_recov = c(0, diff(cum_recov))
+    , daily_deaths = c(0, diff(cum_deaths))
+    , daily_recov_death_ratio = daily_recov/daily_deaths
+  )
+
+
+%>%
+  group_by(
+    date
+  ) %>%
+  summarise(
+    cum_recov_death_ratio = sum(cum_recov_death_ratio, na.rm = TRUE)
+    , daily_recov_death_ratio = sum(daily_recov_death_ratio, na.rm = TRUE)
+  ) %>%
+  gather(metric, n, cum_recov_death_ratio, daily_recov_death_ratio, factor_key = TRUE)
+
+ggplot(a, aes(x=date, y = n, color=metric)) +
+  geom_line()
+
+
+# Moving average plot
+
+library(zoo)
+
+# Recovery to death ratio
+ctry = c('US', 'Bangladesh')
+
+a <- df %>% filter(country %in% ctry) %>%
+  mutate(
+    cum_recov = c(0, diff(cum_recov))
+    , cum_deaths = c(0, diff(cum_deaths))
+    # , daily_recov_death_ratio = daily_recov/daily_deaths
+  ) %>%
+  gather(metric, n, cum_recov, cum_deaths, factor_key = TRUE)
+
+
+a <- df %>%
+  mutate(
+    daily_confirmed = c(0, diff(cum_confirmed))
+    , daily_recov = c(0, diff(cum_recov))
+    , daily_deaths = c(0, diff(cum_deaths))
+  ) %>%
+  group_by(date) %>%
+  summarise(
+    daily_confirmed = sum(daily_confirmed)
+    , daily_recov = sum(daily_recov)
+    , daily_deaths = sum(daily_deaths)
+    , daily_recov_death_ratio = (daily_recov/daily_deaths)
+    # , daily_recov_death_ratio = daily_recov/daily_deaths
+  ) %>%
+  gather(metric, n, daily_recov, daily_confirmed, daily_deaths, factor_key = TRUE)
+
+
+ggplot(a, aes(x=date, y=n, color=metric))+
+  # geom_point(aes(y=rollmean(n, 7, na.pad=TRUE))) +
+  geom_point() +
+  geom_smooth()
