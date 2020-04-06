81bfa76ecd958490fe54de51bb83d087b468b4fc
raheems <raheem@gmail.com>
2020-04-05 21:18:23 -0400

    revised usa report

--- covid19_usa.R
+++ covid19_usa.R
@@ -1,8 +1,15 @@
 # Mon Mar 30 19:53:38 2020 ------------------------------
 # Bangladesh unofficial
 
-# install.packages('gsheet')
-library(tidyverse)
+# Load packages
+require(ggplot2)
+require(ggrepel)
+require(tidyverse)
+require(lubridate)
+require(knitr)
+require(kableExtra)
+library(gsheet)
+library(scales)
 
 
 # RUN covid_update_data.R to update the data
@@ -10,13 +17,13 @@ us_state_data <- "https://raw.githubusercontent.com/nytimes/covid-19-data/master
 df <- read_csv(us_state_data)
 
 
-df <- df %>%
+usa <- df %>%
   rename(
     cum_cases = cases
     , cum_deaths = deaths
   ) %>%
   arrange(state, date)
 
-data_last_refreshed = max(df$date)
+data_last_refreshed = max(usa$date)
 data_last_refreshed
 
